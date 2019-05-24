a158ffa994a21d1b59f92c9aa122117d53ea872a
Christoph Stanik <stanik@informatik.uni-hamburg.de>
2019-05-24 14:33:39 +0200

    removed unused logs

--- src/store/getters.js
+++ src/store/getters.js
@@ -8,9 +8,6 @@ export const loggedIn = state => {
   return state.loggedIn;
 };
 export const userConfigurationTwitterAccounts = state => {
-  console.log("userConfigurationTwitterAccounts", state.userConfiguration);
-  console.log("userConfigurationTwitterAccounts", state.userConfiguration.twitter_accounts);
-
   return state.userConfiguration.twitter_accounts;
 };
 export const filteredTweets = state => {
