f9c66365ff6ff8d100f7a3088fac30f25cc62b74
Christoph Stanik <stanik@informatik.uni-hamburg.de>
2019-06-05 16:35:55 +0200

    extracted routes to constants

--- src/routes.js
+++ src/routes.js
@@ -4,36 +4,47 @@ import DashboardHome from './components/DashboardHome.vue';
 import LoginHome from './components/LoginHome.vue';
 import SettingsHome from './components/SettingsHome.vue';
 import TweetCategoryHome from './components/TweetCategoryHome.vue';
+
+export const ROUTE_EMPTY = "";
+export const ROUTE_DEFAULT = "/";
+export const ROUTE_ALL = "*";
+export const ROUTE_DASHBOARD = "/dashboard";
+export const ROUTE_LOGIN = "/login";
+export const ROUTE_PROBLEM_REPORTS = "/problemReports";
+export const ROUTE_INQUIRIES = "/inquiries";
+export const ROUTE_COMPARISON = "/comparison";
+export const ROUTE_SETTINGS = "/settings";
+
 export const routes = [{
-    path: '',
-    redirect: '/dashboard'
+    path: ROUTE_EMPTY,
+    redirect: ROUTE_DASHBOARD
   },
   {
-    path: '/dashboard',
+    path: ROUTE_DASHBOARD,
     component: DashboardHome
   },
   {
-    path: '/login',
+    path: ROUTE_LOGIN,
     component: LoginHome
   },
   {
-    path: '/problemReports',
+    path: ROUTE_PROBLEM_REPORTS,
     component: TweetCategoryHome
   },
   {
-    path: '/inquiries',
+    path: ROUTE_INQUIRIES,
     component: TweetCategoryHome
   },
   {
-    path: '/comparison',
+    path: ROUTE_COMPARISON,
     component: CompetitorComparisonHome
   },
   {
-    path: '/settings',
+    path: ROUTE_SETTINGS,
     component: SettingsHome
   },
   {
-    path: '*',
-    redirect: '/dashboard'
+    path: ROUTE_ALL,
+    redirect: ROUTE_DASHBOARD
   }
 ];
